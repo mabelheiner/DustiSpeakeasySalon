8f54233ebd9a72302ff254ff314149d33bc44ba0
Mabel Heiner <hei18006@byui.edu>
2026-03-22 15:43:29 -0600

    removed inline styling, added sections instead of divs, added specific classnames for the tables and layout, added a media query to adjust for mobile.

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <>
-    <header className='home-header' style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+    <header className='home-header'>
       <section className='home-image'>
-        <img src={salonLogoImage} alt="Logo for Dusti's Speakeasy Salon" style={{maxHeight: 150, borderRadius: 8}} />
+        <img src={salonLogoImage} alt="Logo for Dusti's Speakeasy Salon" />
       </section>
-      <section className='home-bio' style={{background: 'lightgray', opacity: .75, borderRadius: 8, margin: 8, padding: 8}}>
+      <section className='home-bio'>
         <h1>Welcome to Dusti's Speakeasy Salon</h1>
         <p> 
           Welcome to Dusti's Speakeasy! Rensselaer’s spot for good cuts, and good company. We’re a walk-in-friendly salon where the conversation flows as easily as the clippers. <br />
@@ -22,94 +22,76 @@ function App() {
           </p>
       </section>
       </header>
-      <main className='main-menu' style={{background: 'lightgray', opacity: .75, borderRadius: 8, padding: 8, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '60%', margin: 'auto'}}>
+      <main className='main-menu'>
         <h1>Salon Price List</h1>
-
-        {/* <table style={{}}>
-          <thead style={{textAlign: 'center'}}>
-            <h2 style={{margin: 'auto'}}>Cut & Finish</h2>
-          </thead>
-
-          <tbody style={{}}>
-            <tr>
-              <td>Standard Cut</td>
-              <td>$25</td>
-            </tr>
-
-            <tr>
-              <td>Standard Cut</td>
-              <td>$25</td>
-            </tr>
-          </tbody>
-        </table> */}
 
         <h2>Cut & Finish</h2>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Standard Cut</p>
           <p>$25</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Hero Cut</p>
           <p>$20</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Buz Cut</p>
           <p>$15</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Beard Tailor</p>
           <p>$12</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Shampoo</p>
           <p>$10</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Blowout</p>
           <p>$20+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='haircut-and-finishes-table-row'>
           <p>Hot Tools</p>
           <p>$10+</p>
-        </div>
+        </section>
 
         <h2>Color Services</h2>
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>All Over Color</p>
           <p>$70+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>Partial Highlights</p>
           <p>$75+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>Full Highlights</p>
           <p>$120+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>Root Touchup</p>
           <p>$60+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>Conditioning Treatment</p>
           <p>$15+</p>
-        </div>
+        </section>
 
-        <div className='table-row'>
+        <section className='color-services-table-row'>
           <p>Protein Pack</p>
           <p>$30+</p>
-        </div>
+        </section>
 
         
       </main>
